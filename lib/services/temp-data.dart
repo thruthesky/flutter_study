f16856fe45de5403757b2f630054de48fca0e9b7
JaeHo Song <thruthesky@gmail.com>
2020-04-25 20:08:07 +0800

    adding basic code & temp data

---
+++ lib/services/temp-data.dart
@@ -0,0 +1,66 @@
+Map<String, Map<String, dynamic>> tempWords = {
+  'apple': {
+    'meaning': ['사과'],
+    'star': 3,
+  },
+  'banana': {
+    'meaning': ['바나나'],
+    'star': 3,
+  },
+  'cherry': {
+    'meaning': ['체리'],
+    'star': 1,
+  },
+  'dragon': {
+    'meaning': ['용'],
+    'star': 2,
+  },
+  'effect': {
+    'meaning': ['영향', '결과', '효과'],
+    'star': 3,
+  },
+  'evidence': {
+    'meaning': ['증거', '흔적'],
+    'star': 2,
+  },
+  'explicit': {
+    'meaning': ['분명한', '명쾌한'],
+    'star': 3,
+  },
+  'influence': {
+    'meaning': ['영향', '영향력'],
+    'star': 2,
+  },
+  'internalization': {
+    'meaning': ['내면화', '내재화'],
+    'start': 0,
+  },
+  'agreement': {
+    'meaning': ['협정', '합의', '동의', '승낙'],
+    'star': 2
+  },
+  'consistency': {
+    'meaning': ['한결같음', '일관성'],
+    'star': 1
+  },
+  'concurrence': {
+    'meaning': ['동의', '의견일치', '동시발생'],
+    'star': 0
+  },
+  'capable': {
+    'meaning': ['유능한', '~을 할 수 있는'],
+    'star': 2
+  },
+  'availability': {
+    'meaning': ['유효성', '유용성', '효용', '가능성', '이용 할 수 있는 것'],
+    'star': 0
+  },
+  'proximity': {
+    'meaning': ['가까움', '근접'],
+    'star': 0
+  },
+  'exotic': {
+    'meaning': ['외국의', '이색적인'],
+    'star': 1
+  }
+};
